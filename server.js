bfa50743a48a615bf445a9b8211be60fdf01ba36
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-11-14 00:35:36 -0600

    add middleware and listening notification

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+let db = require('./db/db.json')
+
+// Middleware for parsing JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
+
+// Listening notification within console
+app.listen(PORT, () => {
+    console.log(`App listening at http//localhost:${PORT}`)
+});
