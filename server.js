c41be02804cc6618ca193b50fa53fd5091861d2c
baelindsay <109304038+baelindsay@users.noreply.github.com>
2023-01-20 00:28:32 -0600

    delete notes get

--- server.js
+++ server.js
@@ -34,10 +34,6 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-});
-
 // Listening notification within console
 app.listen(PORT, () => {
     console.log(`App listening at http//localhost:${PORT}`)
